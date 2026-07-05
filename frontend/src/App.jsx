3c0048b4487b1cec742e4c25c04364e4de64e13d
kasra <khankasra82@gmail.com>
2026-07-05 22:22:49 +0330

    themeselector

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,22 +10,29 @@ import CallPage from './pages/CallPage.jsx';
 import toast, { Toaster } from "react-hot-toast";
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+
 import PageLoader from './components/PageLoader.jsx';
 import useAuthUser from './hooks/useAuthUser.js';
+import Layout from './components/layout.jsx';
+import { useThemeStore } from './store/useThemeStore.js';
 
 const App = () => {
   const { isLoading, authUser } = useAuthUser();
 
   const isAuthenticated = Boolean(authUser)
   const isOnboarded = authUser?.isOnboarded
+  const { theme} = useThemeStore();
 
   // if (isLoading) return <PageLoader />;
 
   return (
-    <div className=" h-screen text-5xl " data-theme="coffee" >
+    <div className=" h-screen text-5xl " data-theme={theme}>
       <Routes>
-        <Route path='/' element={isAuthenticated && isOnboarded ?
-          (<HomePage />) :
+        <Route path='/' element={isAuthenticated && isOnboarded ? (
+          <Layout showSidebar={true}>
+            <HomePage />
+          </Layout>
+        ) :
           (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />)}></Route>
 
         <Route path='/signup' element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />}></Route>
@@ -34,7 +41,7 @@ const App = () => {
         <Route path='/call' element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}></Route>
         <Route path='/chat' element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />}></Route>
         <Route path='/onboarding' element={<OnboardingPage />}></Route>
-        {/* <Route path='/onboarding' element={isAuthenticated ? <OnboardingPage /> : <Navigate to="/login" />}></Route> */}
+        <Route path='/onboarding' element={isAuthenticated ? <OnboardingPage /> : <Navigate to="/login" />}></Route>
       </Routes>
       <Toaster />
     </div>
