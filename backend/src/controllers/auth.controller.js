67c08582ecf8f782b4f4fff2ab0de4f59b0cb794
kasra <khankasra82@gmail.com>
2026-07-18 21:54:57 +0330

    .

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -90,7 +90,7 @@ export async function login(req, res) {
         res.cookie("jwt", token, {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true, // prevent XSS attacks,
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // needed for cross-domain (Vercel + Render) cookies
+            // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // needed for cross-domain (Vercel + Render) cookies
             secure: process.env.NODE_ENV === "production",
         });
 
